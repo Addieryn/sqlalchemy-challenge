b0e0f1d08c8b0ed975dde44c6f2b386f6af3126f
Addieryn <aarondehmlow@yahoo.com>
2024-05-07 12:56:04 -0700

    finished code

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # sqlalchemy-challenge
 Utilized Xpert learning for histogram legend: plt.legend(["tobs"], loc="upper right") 
-Reread the instruction to see I didn't actually need that line of code
+~Reread the instruction to see I didn't actually need that line of code~
+looked up a way(stack overflow) to read some results better by making a dictionary to present findings in one line. Though I wasnt eble to cleanly do it with the last ones.
+
+Had some help from Tutor Justin with the <start>/<end> of the project, He helped explain what the "<>" did.
+He also gave me a suggestion(if I wanted to learn more) on how to use html/css in flask, which I use to make it a little nicer. I already was using links(I prefer new tabs)
+
+I added a default date range for start/end so the page doesn't default in "null" and the input format would be simple to understand and change.
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,104 @@
 # Import the dependencies.
+from flask import Flask, jsonify, render_template
 
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
+Base.classes.keys()
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    last_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= last_date).all()
+    
+    all_results = {}
+    for date, prcp in results:
+        all_results[date] = prcp
+    
+    session.close()
+
+    return jsonify(all_results)
+
+@app.route("/api/v1.0/stations")
+def Stations():
+    results = session.query(Measurement.station).group_by(Measurement.station).all()
+
+    all_names = list(np.ravel(results))
+
+    session.close()
+
+    return jsonify(all_names)
+
+@app.route("/api/v1.0/tobs")
+def Temperature():
+    last_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results =  session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= last_date).filter(Measurement.station == 'USC00519281').all()
+
+    all_results = {}
+    for date, tobs in results:
+        all_results[date] = tobs
+
+    session.close()
+        
+    return jsonify(all_results)
+
+@app.route("/api/v1.0/<start>")
+def input_start(start):
+    results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date 
+    >= start).first()
+#results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= start).first()
+
+    session.close()
+
+    return jsonify(list(results))
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).first()
+#results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= start).filter(Measurement.date <= end).first()
+    session.close()
+
+    return jsonify(list(results))
+   
+if __name__ == "__main__":
+    app.run(debug=True)
